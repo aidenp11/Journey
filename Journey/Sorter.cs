8c415d38ffad64acfae1a6b9b589c0d9c0579cba
aidenp11 <129340716+aidenp11@users.noreply.github.com>
2024-04-03 04:00:42 -0600

    tests and selection sort and insertion sort

--- Journey/Sorter.cs
+++ Journey/Sorter.cs
@@ -27,5 +27,43 @@ namespace SortingLibrary
 				}
 			} while (moved > 0);
 		}
+
+		public static void SelectionSort(int[] numbers)
+		{
+			for (int i = 0; i < numbers.Length; i++)
+			{
+				int selected = i;
+
+				for (int o = i + 1; o < numbers.Length; o++)
+				{
+					if (numbers[selected] > numbers[o])
+					{
+						int og = numbers[selected];
+						numbers[selected] = numbers[o];
+						numbers[o] = og;
+						o++;
+
+					}
+				}
+			}
+		}
+
+		public static void InsertionSort(int[] numbers)
+		{
+			for (int i = 1; i < numbers.Length; i++)
+			{
+				int inserted = i;
+				for (int o = i; o > -1; o--)
+				{
+					if (numbers[inserted] < numbers[o])
+					{
+						int og = numbers[o];
+						numbers[o] = numbers[inserted];
+						numbers[inserted] = og;
+						inserted--;
+					}
+				}
+			}
+		}
 	}
 }
--- Journey/Test.cs
+++ Journey/Test.cs
@@ -1,5 +1,6 @@
+using System.Globalization;
 using System.Reflection.Metadata.Ecma335;
-
+using System.Runtime.CompilerServices;
 using Sorter = SortingLibrary.Sorter;
 namespace Tests
 {
@@ -41,9 +42,12 @@ namespace Tests
 			bool temp = Greater(10, 10);
 			Assert.AreEqual(temp, false);
 		}
-		
-		//bubble sort tests
+
+		// ***** sort tests *****
+
 		int[] numbers = { 4, 5, 2, 3, 1 };
+
+		// ***** bubbles *****
 
 		[TestMethod]
 		public void BubbleTest1()
@@ -78,6 +82,111 @@ namespace Tests
 		{
 			Sorter.BubbleSort(numbers);
 			Assert.AreEqual(numbers[4], 5);
+		}
+
+		// ***** selections *****
+
+		[TestMethod]
+		public void SelectionTest1()
+		{
+			Sorter.SelectionSort(numbers);
+			Assert.AreEqual(numbers[0], 1);
+		}
+
+		[TestMethod]
+		public void SelectionTest2()
+		{
+			Sorter.SelectionSort(numbers);
+			Assert.AreEqual(numbers[2], 3);
+		}
+
+		[TestMethod]
+		public void SelectionTest3()
+		{
+			Sorter.SelectionSort(numbers);
+			Assert.AreEqual(numbers[4], 5);
+		}
+
+		[TestMethod]
+		public void SelectionTest4()
+		{
+			Sorter.SelectionSort(numbers);
+			Assert.AreEqual(numbers[1], 2);
+		}
+
+		[TestMethod]
+		public void SelectionTest5()
+		{
+			Sorter.SelectionSort(numbers);
+			Assert.AreEqual(numbers[3], 4);
+		}
+
+		[TestMethod] 
+		public void SelectionTest6()
+		{
+
+			Sorter.SelectionSort(numbers);
+			Assert.IsTrue(numbers[0] < numbers[1]);
+		}
+
+		[TestMethod]
+		public void SelectionTest7()
+		{
+
+			Sorter.SelectionSort(numbers);
+			Assert.IsTrue(numbers[4] > numbers[3]);
+		}
+
+		// ***** insertions *****
+
+		[TestMethod]
+		public void InsertionTest1()
+		{
+			Sorter.InsertionSort(numbers);
+			Assert.AreEqual(numbers[0], 1);
+		}
+
+		[TestMethod]
+		public void InsertionTest2()
+		{
+			Sorter.InsertionSort(numbers);
+			Assert.AreEqual(numbers[2], 3);
+		}
+
+		[TestMethod]
+		public void InsertionTest3()
+		{
+			Sorter.InsertionSort(numbers);
+			Assert.AreEqual(numbers[4], 5);
+		}
+
+		[TestMethod]
+		public void InsertionTest4()
+		{
+			Sorter.InsertionSort(numbers);
+			Assert.AreEqual(numbers[1], 2);
+		}
+
+		[TestMethod]
+		public void InsertionTest5()
+		{
+			Sorter.InsertionSort(numbers);
+			Assert.AreEqual(numbers[3], 4);
+		}
+
+		[TestMethod]
+		public void InsertionTest6()
+		{
+			Sorter.InsertionSort(numbers);
+			Assert.AreEqual(numbers.Length, 5);
+		}
+
+		[TestMethod]
+		public void InsertionTest7()
+		{
+			int[] newn = numbers.Append(-1).ToArray();
+			Sorter.InsertionSort(newn);
+			Assert.AreEqual(newn[0], -1);
 		}
 	}
 }
